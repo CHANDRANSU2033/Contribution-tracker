5200bad8f10609c8f82e5d7015cdaf191b6b70cf
ChangaBaliye <cse.22bcsd19@silicon.ac.in>
2025-10-18 11:24:58 +0530

    yup

--- open-source-tracker/src/lib/types.ts
+++
@@ -1,8 +0,0 @@
-export type ContributionStats = {
-    totalContributions: number
-    uniqueRepos: number
-    dailyActivity: Array<{ date: string; count: number }>
-    repoDistribution: Array<{ repo: string; count: number }>
-    activeDays?: number
-    currentStreak?: number
-  }
